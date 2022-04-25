0be1fa73b50e5e673703f9ae407babf2d60cec35
NaikyDing <74ding@gmail.com>
2022-04-25 15:06:22 +0800

    fix: model type #組員f

--- model/post.js
+++ model/post.js
@@ -12,7 +12,7 @@ const postSchema = new Schema(
       default: '這是頭像 url',
     },
     content: {
-      type: 'String',
+      type: String,
       required: [true, '請輸入貼文內容'],
     },
     image: {
