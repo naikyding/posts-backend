3132c7cb4293c26078e1a273d7d05c974a72f230
NaikyDing <74ding@gmail.com>
2022-04-25 13:37:43 +0800

    feat: 共用響應功能 #組員c

---
+++ utiils/responseHandler.js
@@ -0,0 +1,38 @@
+const headers = require('./config/headers')
+
+const successHandler = async ({
+  res,
+  statusCode = '200',
+  message = '操作成功',
+  data = [],
+}) => {
+  res.writeHead(statusCode, headers)
+  res.end(
+    JSON.stringify({
+      status: 'Success',
+      message,
+      data,
+    })
+  )
+}
+
+const errorHandler = async ({
+  res,
+  statusCode = '400',
+  message = '操作失敗',
+  errors = [],
+}) => {
+  res.writeHead(statusCode, headers)
+  res.end(
+    JSON.stringify({
+      status: 'Error',
+      message,
+      errors: [],
+    })
+  )
+}
+
+module.exports = {
+  successHandler,
+  errorHandler,
+}
